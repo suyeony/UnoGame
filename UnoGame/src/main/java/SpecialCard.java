e9466003a43323eeeb14db69055ddf0122bbc8e7
Giuliana Grecco <giu.grecco@gmail.com>
2019-12-20 20:05:18 -0700

    Adding SpecialCard

--- UnoGame/src/main/java/SpecialCard.java
+++ UnoGame/src/main/java/SpecialCard.java
@@ -1,2 +1,11 @@
-public class SpecialCard {
+public class SpecialCard extends Card {
+ private static Rule rule;
+
+    public static void setRule(Rule rule) {
+        SpecialCard.rule = rule;
+    }
+
+    public static Rule getRule() {
+        return rule;
+    }
 }
