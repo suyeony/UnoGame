24b26eefbf2dc85e994ca6bb7aa488962cba646c
Giuliana Grecco <giu.grecco@gmail.com>
2019-12-20 19:58:06 -0700

    Adding Player Class

--- UnoGame/src/main/java/Player.java
+++ UnoGame/src/main/java/Player.java
@@ -1,2 +1,33 @@
 public class Player {
+    static String name;
+    static Deck deck;
+    static boolean turn;
+
+    Player(String name, Deck deck) {
+        this.name = name;
+        this.deck = deck;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setTurn(boolean turn){
+        this.turn = turn;
+    }
+    public boolean getTurn(){
+        return turn;
+    }
 }
