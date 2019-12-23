f16354af0d09203997a135736c31e33f1e69d6f8
Giuliana Grecco <giu.grecco@gmail.com>
2019-12-23 16:53:14 -0700

    Adding +4 and wild cards

--- UnoGame/src/main/java/Deck.java
+++ UnoGame/src/main/java/Deck.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Deck {
     private List<Card> cards;
-//    private final int quantityCards = 76;
+    //    private final int quantityCards = 76;
 //    private final int quantitySpecialCards = 32;
     private final Map<Integer, String> colors =
             Map.of(1, "red", 2, "blue", 3, "green", 4, "yellow");
@@ -11,11 +11,12 @@ public class Deck {
         cards = new ArrayList<>();
         for (String color : colors.values()) {
             MakingCards(cards, color);
+            MakingSpecialCards(cards, color, Actions.ADD_TWO, "+2",2);
+            MakingSpecialCards(cards, color, Actions.SKIP_TURN, "Skip",2);
+            MakingSpecialCards(cards, color, Actions.REVERSE, "reverse",2);
         }
-//        MakingSpecialCards(cards, "red", Actions.ADD_TWO, "+2");
-//        MakingSpecialCards(cards, "blue", Actions.ADD_TWO, "+2");
-//        MakingSpecialCards(cards, "green", Actions.ADD_TWO, "+2");
-//        MakingSpecialCards(cards, "red", Actions.ADD_TWO, "+2");
+        MakingSpecialCards(cards,"none",Actions.CHOOSE_COLOR,"wild",4);
+        MakingSpecialCards(cards,"none",Actions.ADD_FOUR,"+4",4);
     }
 
     private void MakingCards(List<Card> cards, String color) {
@@ -29,11 +30,14 @@ public class Deck {
         }
     }
 
-    private void MakingSpecialCards(List<Card> cards, String color, Actions action, String value) {
-        var specialCard = new SpecialCard();
-        specialCard.setColor(color); // 8 none 24 color, 6blue, 6green, 6yellow, 6red
-        specialCard.setValue("+2"); // +2, wild, +4, reverse, skip   24 + 8
-        specialCard.setActions(Actions.ADD_FOUR);
+    private void MakingSpecialCards(List<Card> cards, String color, Actions action, String value, int n) {
+        for (int index = 0; index < n; index++) {
+            var specialCard = new SpecialCard();
+            specialCard.setColor(color);
+            specialCard.setValue(value);
+            specialCard.setActions(action);
+            cards.add(specialCard);
+        }
     }
 
     public List<Card> ShowCards() {
