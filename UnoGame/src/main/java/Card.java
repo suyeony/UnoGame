4995b95d3c9fd7ac5a67d8040a3e492dbd5d9a71
Suyeon Yang <yan17002@byui.edu>
2019-12-23 13:17:05 -0700

    started the card class

--- UnoGame/src/main/java/Card.java
+++ UnoGame/src/main/java/Card.java
@@ -1,2 +1,26 @@
 public class Card {
+    private static String value;
+    private static String color;
+    
+    public Card(String cardValue, String cardColor) {
+        this.value = cardValue;
+        this.color = cardColor;
+    }
+    
+    public static void setValue(String value) {
+        Card.value = value;
+    }
+    
+    public static getValue() {
+        return value;
+    }
+    
+    public static void setColor(String color) {
+        Card.color = color;
+    }
+    
+    public getColor() {
+        return color;
+}
+    
 }
