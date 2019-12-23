db4510d675bcbd090c52008652995e0b69f53d88
Suyeon Yang <yan17002@byui.edu>
2019-12-23 13:28:58 -0700

    started deck class

--- UnoGame/src/main/java/Deck.java
+++ UnoGame/src/main/java/Deck.java
@@ -1,2 +1,18 @@
+import java.util.*;
 public class Deck {
+    List<String> listOfCard;
+    
+    public drawCard() {
+    
+}
+    public mixCard() {
+    
+}
+    public giveCard() {
+    
+}
+    public startCard() {
+    
+}
+    
 }
